d0bd196fb1d39fd3d96ff6e52d2c48f5cf8cdd74
Ravi Decover <ravidecover@Ravis-MacBook-Pro-2.local>
2023-06-08 17:29:47 -0700

    Shared Elements added.
    - Input Element
    - Parsed Output
    - S3 Util (Read/Write)

---
+++ shared/input_elem.py
@@ -0,0 +1,72 @@
+from typing import Optional
+
+class InputElem:
+    """
+    Represents of each row of the CSV that is read by Crawlumbus.
+    """
+    def __init__(self, title: str, url: str, allowed_domains: Optional[str] = None,
+                 frequency: str = 'once', jurisdiction: Optional[str] = None):
+        self._title = title
+        self._url = url
+        self._allowed_domains = allowed_domains if allowed_domains else self.get_root_domain(url)
+        self._frequency = frequency
+        self._jurisdiction = jurisdiction
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value: str):
+        self._title = value
+
+    @property
+    def url(self):
+        return self._url
+
+    @url.setter
+    def url(self, value: str):
+        self._url = value
+        self._allowed_domains = self.get_root_domain(value)
+
+    @property
+    def allowed_domains(self):
+        return self._allowed_domains
+
+    @allowed_domains.setter
+    def allowed_domains(self, value: Optional[str]):
+        self._allowed_domains = value if value else self.get_root_domain(self._url)
+
+    @property
+    def frequency(self):
+        return self._frequency
+
+    @frequency.setter
+    def frequency(self, value: str):
+        self._frequency = value
+
+    @property
+    def jurisdiction(self):
+        return self._jurisdiction
+
+    @jurisdiction.setter
+    def jurisdiction(self, value: Optional[str]):
+        self._jurisdiction = value
+
+    @staticmethod
+    def get_root_domain(url: str):
+        from urllib.parse import urlparse
+        netloc = urlparse(url).netloc
+        return '.'.join(netloc.split('.')[-2:])
+
+    def __str__(self):
+        return f'InputElem({self._title}, {self._url}, {self._allowed_domains}, {self._frequency}, {self._jurisdiction})'
+
+    def __dict__(self):
+        return {
+            'title': self._title,
+            'url': self._url,
+            'allowed_domains': self._allowed_domains,
+            'frequency': self._frequency,
+            'jurisdiction': self._jurisdiction,
+        }
---
+++ shared/s3_client.py
@@ -0,0 +1,57 @@
+import os
+
+import boto3
+import logging
+
+from urllib.parse import urlparse, unquote
+
+
+def extract_file_name_from_s3_url(s3_url: str) -> str:
+    """
+    Extracts the name of the file from s3_url.
+    :param s3_url:
+    :return: The name of the file.
+    """
+    parsed_url = urlparse(s3_url)
+    path = parsed_url.path
+    file_name = path.rsplit('/', 1)[-1]  # Extract the last part after the last slash
+    decoded_file_name = unquote(file_name)  # Decode URL-encoded file name
+    return decoded_file_name
+
+
+class S3Client:
+    """
+    This class is responsible for storing and retrieving files from S3.
+    """
+    def __init__(self):
+        self.bucket_name = 'decover-development'
+        self.region_name = 'us-east-1'
+        self.aws_access_key_id = 'AKIAVVK77GPEFCABAKVM'
+        self.aws_secret_access_key = '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'
+        self.file_expiration_seconds = 3600
+        self.s3 = boto3.client('s3',
+                               region_name=self.region_name,
+                               aws_access_key_id=self.aws_access_key_id,
+                               aws_secret_access_key=self.aws_secret_access_key)
+
+    def put_file(self, src_file_name: str, target_file_name: str) -> str:
+        logging.info(f'Uploading {src_file_name} to S3')
+        self.s3.upload_file(src_file_name, self.bucket_name, target_file_name)
+        url = self.s3.generate_presigned_url('get_object',
+                                             Params={'Bucket': self.bucket_name,
+                                                     'Key': target_file_name},
+                                             ExpiresIn=self.file_expiration_seconds)
+        return url
+
+    def get_file(self, file_name: str) -> str:
+        # Extract the file name from the URL.
+        extracted_file_name = extract_file_name_from_s3_url(file_name)
+        # Get the extension of the file.
+        file_extension = os.path.splitext(extracted_file_name)[1]
+        # Create a temp file name.
+        tmp_file_name = f'temp{file_extension}'
+        logging.info(f'Downloading {extracted_file_name} from S3')
+        # Download the file from S3.
+        extracted_file_name = extract_file_name_from_s3_url(file_name)
+        self.s3.download_file(self.bucket_name, extracted_file_name, tmp_file_name)
+        return tmp_file_name
