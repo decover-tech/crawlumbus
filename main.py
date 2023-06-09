58486b7dc0a4204b76a6986ddb5d68d7cebcf98e
Ravi Decover <ravidecover@Ravis-MacBook-Pro-2.local>
2023-06-09 08:30:03 -0700

    Update to comments

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Use the WebSiteCrawlerScrapy class to crawl an input website.
 from crawler.website_crawler_scrapy import WebSiteCrawlerScrapy
 
+# This is the main driver file. It should be broken down into the following steps:-
+# Step I: Read CSV from an S3 file.
+# Step 2: For each row in the CSV, crawl the website. This should be done in parallel.
+# Step 3: Emit out WebPageInfo objects to an S3 bucket.
+# Step 4: Write the updated state to a Postgres table.
+# Step 5: Read the contents and send them to Decover index. This should be done in parallel.
+
 if __name__ == '__main__':
     # Step I: Get the input website.
     website = 'https://doj.gov.in/'
