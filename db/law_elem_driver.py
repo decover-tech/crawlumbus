5d9772b8b8248b57cb6ae224d571a508e5a17ec2
Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>
2023-06-25 17:38:26 +0530

    Add page to view the status of laws

---
+++ db/law_elem_driver.py
@@ -0,0 +1,24 @@
+from db.law_elem import LawElemModel
+
+
+class LawElemDriver:
+    @staticmethod
+    def add_law(db, law_name, jurisdiction, category, sub_category, url, file_name, title):
+        new_law = LawElemModel(law_name, jurisdiction,
+                               category, sub_category, url, file_name, title)
+        db.session.add(new_law)
+        db.session.commit()
+
+    @staticmethod
+    def get_law(db, law_name):
+        law = db.session.query(LawElemModel).filter_by(
+            law_name=law_name).first()
+        return law
+
+    @staticmethod
+    def update_law(db, law_name, **kwargs):
+        law = db.session.query(LawElemModel).filter_by(
+            law_name=law_name).first()
+        for key, value in kwargs.items():
+            setattr(law, key, value)
+        db.session.commit()
